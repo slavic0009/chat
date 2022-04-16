fe6961b90da9f47e3da6f06f62717022bc9cb418
Вячеслав Суханов <slavinapochta77@outlook.com>
2022-04-16 14:48:12 +0300

    Добавьте файлы проекта.

---
+++ Чатус/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Чатус
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(828, 89);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(831, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Логин";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(831, 52);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 29);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Пароль";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(16, 487);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(803, 28);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(825, 487);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Отправить сообщение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(828, 174);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(233, 29);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(828, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Поиск сообщения";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(13, 15);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(805, 466);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.UseCompatibleStateImageBehavior = false;
+            this.textBox2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Чат:";
+            this.columnHeader1.Width = 597;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(135, 28);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 530);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Чатик в локальной сеточки";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Чатус/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Media;
+
+namespace Чатус
+{
+    public partial class Form1 : Form
+    {
+        static Socket Sockett = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static int t = 0;
+        static string message;
+        static List<string> Slines = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 667);
+            try
+            {
+                Socket Sockit = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Sockit.Connect(ipPoint);
+
+                byte[] buffer = Encoding.UTF8.GetBytes(textBox1.Text);
+                int bytesSent = Sockit.Send(buffer);
+
+                Thread.Sleep(800);
+
+                byte[] buffer2 = Encoding.UTF8.GetBytes(textBox3.Text);
+                int bytesSent1 = Sockit.Send(buffer2);
+
+                Thread.Sleep(600);
+
+
+                byte[] buffer1 = new byte[8196];
+                int bytesRec = Sockit.Receive(buffer1);
+                string answer = Encoding.UTF8.GetString(buffer1, 0, bytesRec);
+
+                if (answer == "123")
+                {
+                    Sockett = Sockit;
+                    Thread ListenThread = new Thread(Listening);
+                    ListenThread.IsBackground = true;
+                    ListenThread.Start();
+                    button1.Enabled = false;
+                    textBox1.Clear();
+                    textBox1.ReadOnly = true;
+                    timer1.Enabled = true;
+                    textBox3.ReadOnly = true;
+                    textBox3.Clear();
+                    textBox4.ReadOnly = false;
+                    button2.Enabled = true;
+                    textBox5.ReadOnly = false;
+                }
+                else
+                {
+                    textBox2.Items.Add(DateTime.Now.ToString("hh:mm") + " Неверный логин или пароль");
+                    lines.Add(DateTime.Now.ToString("hh:mm") + " Неверный логин или пароль" + Environment.NewLine);
+                } 
+            } 
+            catch 
+            {
+                textBox2.Items.Add(DateTime.Now.ToString("hh:mm") + " Сервер недоступен");
+                lines.Add(DateTime.Now.ToString("hh:mm") + " Сервер недоступен" + Environment.NewLine);
+            }
+        }
+        static void Listening()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[8196];
+                    int bytesRec = Sockett.Receive(buffer);
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                    message = data;
+                    t = 1;
+                }
+                catch { }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (t == 1)
+            {
+                textBox2.Items.Add(DateTime.Now.ToString("hh:mm") + " " + message);
+                lines.Add(DateTime.Now.ToString("hh:mm") + " " + message + Environment.NewLine);
+                try
+                {
+                    notifyIcon1.Icon = SystemIcons.WinLogo;
+                    notifyIcon1.BalloonTipText = (message);
+                    notifyIcon1.BalloonTipTitle = "Новое сообщение";
+                    notifyIcon1.ShowBalloonTip(5);
+                }
+                catch { }
+                t = 0;
+            }
+        }
+        static List<string> lines = new List<string>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox4.Text[0] == '#')
+                {
+                    lines.Add(DateTime.Now.ToString("hh:mm") + "Сообщение не может начинаться с символа #");
+                    textBox2.Items.Add(DateTime.Now.ToString("hh:mm") + " Сообщение не может начинаться с символа #");
+                    textBox4.Clear();
+                }
+            }
+            catch { }
+            try
+            {
+                if (textBox4.Text[0] != '#')
+                {
+                    try
+                    {
+
+                        byte[] buffer1 = Encoding.UTF8.GetBytes(textBox4.Text);
+                        Slines.Add(textBox4.Text);
+                        int bytesSent = Sockett.Send(buffer1);
+                        textBox4.Clear();
+                    }
+
+                    catch
+                    {
+                        textBox2.Items.Add(DateTime.Now.ToString("hh:mm") + " Сервер недоступен");
+                        lines.Add(DateTime.Now.ToString("hh:mm") + " Сервер недоступен" + Environment.NewLine);
+                        textBox4.Clear();
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox5.Text != "")
+            {
+                textBox2.Items.Clear();
+                for (int j = 0; j < lines.Count; j++)
+                {
+                    if (lines[j].Contains(textBox5.Text))
+                    {
+                        textBox2.Items.Add(lines[j]);
+                    }
+                }
+            }
+            if(textBox5.Text == "")
+            {
+                textBox2.Clear();
+                for (int j = 0; j < lines.Count; j++)
+                {
+                    textBox2.Items.Add(lines[j]);
+                }
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                button2_Click(sender, e);
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                button1_Click(sender, e);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                byte[] buffer1 = Encoding.UTF8.GetBytes("#");
+                int bytesSent = Sockett.Send(buffer1);
+            }
+            catch { }
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ListViewItem lvi = textBox2.SelectedItems[0];
+                lvi = textBox2.SelectedItems[0];
+                lvi.Remove();
+                lines.Clear();
+                foreach (ListViewItem s in textBox2.Items)
+                {
+                    lines.Add(s.Text);
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+            }
+            if (WindowState == FormWindowState.Normal)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Игра" && textBox3.Text == "в кальмара")
+            {
+                SoundPlayer sp = new SoundPlayer("123.wav");
+                sp.Play();
+            }
+        }
+    }
+}
